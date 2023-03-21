29373ec4509966f29137af5a9adc42310dfead47
Nastia <anasstasiapuhova847@gmail.com>
2023-03-21 13:10:40 +0300

    21.032023

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Деление обыкновенных дробей");
+            Console.Write("Введите первый числитель:");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите первый знаменатель:");
+            double c = double.Parse(Console.ReadLine());
+            Console.Write("Введите второй числитель:");
+            double v = double.Parse(Console.ReadLine());
+            Console.Write("Введите второй знаменатель:");
+            double y = double.Parse(Console.ReadLine());
+            if (x >= 0 && y >= 0)
+
+            {
+                double z = (x / c) % (v / y);
+                Console.WriteLine("z = {0}", z);
+            }
+            else
+            {
+                Console.WriteLine("Нету решения");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
